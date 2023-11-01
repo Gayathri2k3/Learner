06e6bc2bbc61a0082e62697d23dbf810c5370353
Gayathri2k3 <149236114+Gayathri2k3@users.noreply.github.com>
2023-11-01 23:13:20 +0530

    Create .java

---
+++ Evennum/.java
@@ -0,0 +1,9 @@
+public class Evennum{
+    public static void main(String args[]){
+        System.out.println("The First 10 Even numbers are:");
+        for(int i=1;i<=10;i++){
+            if(i%2==0)
+            System.out.println(""+i);
+        }
+    }
+}
